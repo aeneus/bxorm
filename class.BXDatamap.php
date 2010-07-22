dd53803ddfe0472d74c40377f5a3ff4ec5608346
aeneus <noita@yandex.ru>
2010-07-22 23:55:13 +0400

    add message to exception
    Signed-off-by: aeneus <noita@yandex.ru>

--- class.BXDatamap.php
+++ class.BXDatamap.php
@@ -12,7 +12,7 @@ class BXDatamap {
 	 */
 	public function add_column(BXColumn $column) {
 		if (array_key_exists($column->get_field_name(), $this->columns)) {
-			throw new Exception();
+			throw new Exception('Column with same name already exists');
 		}
 		$this->columns[$column->get_field_name()] = $column;
 	}
