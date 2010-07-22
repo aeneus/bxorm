ca7e2f4a6673e713b0496a3e948752a1a72f5aaa
aeneus <noita@yandex.ru>
2010-07-22 19:27:19 +0400

    change: name -> field_name, key -> column_name Signed-off-by: aeneus <noita@yandex.ru>

--- class.BXField.php
+++ class.BXField.php
@@ -1,12 +1,23 @@
 <?
 class BXField {
 	
-	protected $name;
-	protected $key;
+	/**
+	 * Name of field in Bitrix scheme
+	 *
+	 * @var string
+	 */
+	protected $column_name;
 	
-	public function __construct($name, $key) {
-		$this->name = $name;
-		$this->key = $key;
+	/**
+	 * Name of field in domain layer
+	 *
+	 * @var string
+	 */
+	protected $field_name;
+	
+	public function __construct($field_name, $column_name) {
+		$this->field_name = $field_name;
+		$this->column_name = $column_name;
 	}
 	
 	public function get_name() {
