c10fbff19f92c8fd683520e5e6b89d10d5ce97e3
aeneus <noita@yandex.ru>
2010-07-22 19:10:47 +0400

    protected $name, $key; add methods get_name(), get_key() Signed-off-by: aeneus <noita@yandex.ru>

--- class.BXField.php
+++ class.BXField.php
@@ -1,12 +1,20 @@
 <?
 class BXField {
 	
-	public $name;
-	public $key;
+	protected $name;
+	protected $key;
 	
 	public function __construct($name, $key) {
 		$this->name = $name;
 		$this->key = $key;
+	}
+	
+	public function get_name() {
+		return $this->name;
+	}
+	
+	public function get_key() {
+		return $this->key;
 	}
 	
 }
